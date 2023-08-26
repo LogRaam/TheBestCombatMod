061548d245c036af6b3b3d52beae8d7e8270ebb3
LogRaam <gabriel.mailhot@gmail.com>
2023-08-26 15:51:09 -0400

    Try out simplifying Javelin class

--- TheBestCombatMod/Features/Unseat/Weapon/Javelin.cs
+++ TheBestCombatMod/Features/Unseat/Weapon/Javelin.cs
@@ -14,7 +14,8 @@ namespace TheBestCombatMod.Features.Unseat.Weapon
       public readonly DamageTypes DamageType;
       public readonly StrikeType StrikeType;
       public ArmorComponent.ArmorMaterialTypes MaterialType;
-
+      private readonly UnseatOptionReader _option;
+      private int _weaponForce;
 
       public Javelin(in StrikeType strikeType,
                      in DamageTypes damageType,
@@ -23,123 +24,143 @@ namespace TheBestCombatMod.Features.Unseat.Weapon
          StrikeType = strikeType;
          DamageType = damageType;
          MaterialType = materialType;
+         _weaponForce = Runtime.WeaponStaggerForceValue.Javelin;
+         _option = Runtime.Get.UnseatOptionReader;
       }
 
       public int GetResistanceBonus(in string[] loadedOptions)
       {
-         var weaponForce = Runtime.WeaponStaggerForceValue.Javelin;
-
-         var option = Runtime.Get.UnseatOptionReader;
-
-
          if (MaterialType == ArmorComponent.ArmorMaterialTypes.None)
          {
-            weaponForce = (int) (weaponForce * 1.5);
+            _weaponForce = (int) (_weaponForce * 1.5);
             MaterialType = ArmorComponent.ArmorMaterialTypes.Cloth;
          }
 
-         if (MaterialType == ArmorComponent.ArmorMaterialTypes.Cloth)
+         switch (MaterialType)
          {
-            if (StrikeType == StrikeType.Swing)
+            case ArmorComponent.ArmorMaterialTypes.Cloth: return ClothResistanceBonus(loadedOptions, StrikeType, DamageType);
+
+            case ArmorComponent.ArmorMaterialTypes.Leather when StrikeType == StrikeType.Swing:
             {
                if (DamageType == DamageTypes.Cut)
                {
-                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_CLOTH_rN5uX_Value);
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_LEATHER_iK6xO_Value);
                }
 
                if (DamageType == DamageTypes.Blunt)
                {
-                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_CLOTH_tA1vZ_Value);
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_LEATHER_yM4aI_Value);
                }
-            }
 
-            if (StrikeType == StrikeType.Thrust)
+               break;
+            }
+            case ArmorComponent.ArmorMaterialTypes.Leather when StrikeType == StrikeType.Thrust:
             {
                if (DamageType == DamageTypes.Pierce)
                {
-                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_CLOTH_mC9dH_Value);
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_LEATHER_pE8bQ_Value);
                }
 
                if (DamageType == DamageTypes.Blunt)
                {
-                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_CLOTH_kY3gW_Value);
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_LEATHER_sU7nJ_Value);
                }
-            }
-         }
-         else if (MaterialType == ArmorComponent.ArmorMaterialTypes.Leather && StrikeType == StrikeType.Swing)
-         {
-            if (DamageType == DamageTypes.Cut)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_LEATHER_iK6xO_Value);
-            }
 
-            if (DamageType == DamageTypes.Blunt)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_LEATHER_yM4aI_Value);
+               break;
             }
-         }
-         else if (MaterialType == ArmorComponent.ArmorMaterialTypes.Leather && StrikeType == StrikeType.Thrust)
-         {
-            if (DamageType == DamageTypes.Pierce)
+            case ArmorComponent.ArmorMaterialTypes.Chainmail when StrikeType == StrikeType.Swing:
             {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_LEATHER_pE8bQ_Value);
-            }
+               if (DamageType == DamageTypes.Cut)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_CHAINMAIL_nB7eU_Value);
+               }
 
-            if (DamageType == DamageTypes.Blunt)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_LEATHER_sU7nJ_Value);
-            }
-         }
-         else if (MaterialType == ArmorComponent.ArmorMaterialTypes.Chainmail && StrikeType == StrikeType.Swing)
-         {
-            if (DamageType == DamageTypes.Cut)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_CHAINMAIL_nB7eU_Value);
-            }
+               if (DamageType == DamageTypes.Blunt)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_CHAINMAIL_pA3vR_Value);
+               }
 
-            if (DamageType == DamageTypes.Blunt)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_CHAINMAIL_pA3vR_Value);
+               break;
             }
-         }
-         else if (MaterialType == ArmorComponent.ArmorMaterialTypes.Chainmail && StrikeType == StrikeType.Thrust)
-         {
-            if (DamageType == DamageTypes.Pierce)
+            case ArmorComponent.ArmorMaterialTypes.Chainmail when StrikeType == StrikeType.Thrust:
             {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_CHAINMAIL_xF9cV_Value);
-            }
+               if (DamageType == DamageTypes.Pierce)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_CHAINMAIL_xF9cV_Value);
+               }
 
-            if (DamageType == DamageTypes.Blunt)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_CHAINMAIL_zD5tL_Value);
-            }
-         }
-         else if (MaterialType == ArmorComponent.ArmorMaterialTypes.Plate && StrikeType == StrikeType.Swing)
-         {
-            if (DamageType == DamageTypes.Cut)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_PLATE_rC4dP_Value);
-            }
+               if (DamageType == DamageTypes.Blunt)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_CHAINMAIL_zD5tL_Value);
+               }
 
-            if (DamageType == DamageTypes.Blunt)
-            {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_PLATE_tZ7uO_Value);
+               break;
             }
-         }
-         else if (MaterialType == ArmorComponent.ArmorMaterialTypes.Plate && StrikeType == StrikeType.Thrust)
-         {
-            if (DamageType == DamageTypes.Pierce)
+            case ArmorComponent.ArmorMaterialTypes.Plate when StrikeType == StrikeType.Swing:
             {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_PLATE_oX1wM_Value);
-            }
+               if (DamageType == DamageTypes.Cut)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_PLATE_rC4dP_Value);
+               }
 
-            if (DamageType == DamageTypes.Blunt)
+               if (DamageType == DamageTypes.Blunt)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_PLATE_tZ7uO_Value);
+               }
+
+               break;
+            }
+            case ArmorComponent.ArmorMaterialTypes.Plate when StrikeType == StrikeType.Thrust:
             {
-               return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, weaponForce, option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_PLATE_mY8bF_Value);
+               if (DamageType == DamageTypes.Pierce)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_PLATE_oX1wM_Value);
+               }
+
+               if (DamageType == DamageTypes.Blunt)
+               {
+                  return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_PLATE_mY8bF_Value);
+               }
+
+               break;
             }
          }
 
-         return weaponForce;
+         return _weaponForce;
       }
+
+      #region private
+
+      private int ClothResistanceBonus(in string[] loadedOptions, StrikeType strikeType, DamageTypes damageType)
+      {
+         switch (strikeType)
+         {
+            case StrikeType.Swing: return ClothSwingResistance(loadedOptions, damageType);
+            case StrikeType.Thrust: return ClothThrustResistance(loadedOptions, damageType);
+            default: return _weaponForce;
+         }
+      }
+
+      private int ClothSwingResistance(in string[] loadedOptions, DamageTypes damageType)
+      {
+         switch (damageType)
+         {
+            case DamageTypes.Cut: return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_CUT_AGAINST_CLOTH_rN5uX_Value);
+            case DamageTypes.Blunt: return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_SWING_BLUNT_AGAINST_CLOTH_tA1vZ_Value);
+            default: return _weaponForce;
+         }
+      }
+
+      private int ClothThrustResistance(in string[] loadedOptions, DamageTypes damageType)
+      {
+         switch (damageType)
+         {
+            case DamageTypes.Pierce: return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_PIERCE_AGAINST_CLOTH_mC9dH_Value);
+            case DamageTypes.Blunt: return Runtime.UnseatImpactResistance.ResistanceBonus(loadedOptions, _weaponForce, _option.UnseatValues.JAVELIN_THRUST_BLUNT_AGAINST_CLOTH_kY3gW_Value);
+            default: return _weaponForce;
+         }
+      }
+
+      #endregion
    }
 }
